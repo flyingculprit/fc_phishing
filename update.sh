7bb3cf5c7dbf25a9db45e5189572f3cc67901aac
flyingculprit <suryan2021ai@gmail.com>
2024-03-13 23:33:48 +0530

    addes

---
+++ update.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Update Script
+
+
+dependencies() {
+
+command -v git > /dev/null 2>&1 || { echo >&2 "Package GIT is not installed ... Unable to update ..."; exit 1; }
+
+}
+
+script() {
+
+clear
+printf "\n \e[1;92mUpdating \e[1;94mShellPhish\e[1;92m directory ...\n\n"
+sleep 1.5
+cd ..
+rm -rf ShellPhish
+git clone https://github.com/flyingculprit/ShellPhish
+cd ShellPhish
+chmod +x *
+printf "\n\e[1;92m Update Complete ...\n\e[0m"
+
+}
+
+dependencies
+script
