e5cc59ecc17109257436a882da33296a0dcb9849
flyingculprit <suryan2021ai@gmail.com>
2024-03-14 21:05:20 +0530

    New one

--- update.sh
+++ update.sh
@@ -14,9 +14,9 @@ clear
 printf "\n \e[1;92mUpdating \e[1;94mShellPhish\e[1;92m directory ...\n\n"
 sleep 1.5
 cd ..
-rm -rf ShellPhish
-git clone https://github.com/flyingculprit/ShellPhish
-cd ShellPhish
+rm -rf fc_phishing
+git clone https://github.com/flyingculprit/fc_phishing.git
+cd fc_phishing
 chmod +x *
 printf "\n\e[1;92m Update Complete ...\n\e[0m"
 
